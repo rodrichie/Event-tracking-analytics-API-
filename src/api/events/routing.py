d9d300e0ba2883fae73d6cc14dfe68fa97f32f43
rodrichie <nabasarodrick@gmail.com>
2025-12-23 18:47:35 +0300

    Add events API endpoints

---
+++ src/api/events/routing.py
@@ -0,0 +1,94 @@
+import os
+from typing import List
+from fastapi import APIRouter, Depends, HTTPException, Query
+from sqlmodel import Session, select
+from sqlalchemy import case, func
+from timescaledb.hyperfunctions import time_bucket
+from datetime import datetime, timedelta, timezone
+from api.db.session import get_session
+
+from .models import (
+    EventModel, 
+    EventBucketSchema, 
+    EventCreateSchema,
+    get_utc_now
+)
+router = APIRouter()
+
+DEFAULT_LOOKUP_PAGES = [
+        "/", "/about", "/pricing", "/contact", 
+        "/blog", "/products", "/login", "/signup",
+        "/dashboard", "/settings"
+    ]
+
+# Get data here
+# List View
+# GET /api/events/
+@router.get("/", response_model=List[EventBucketSchema])
+def read_events(
+        duration: str = Query(default="1 day"),
+        pages: List = Query(default=None),
+        session: Session = Depends(get_session)
+    ):
+    # a bunch of items in a table
+    os_case = case(
+        (EventModel.user_agent.ilike('%windows%'), 'Windows'),
+        (EventModel.user_agent.ilike('%macintosh%'), 'MacOS'),
+        (EventModel.user_agent.ilike('%iphone%'), 'iOS'),
+        (EventModel.user_agent.ilike('%android%'), 'Android'),
+        (EventModel.user_agent.ilike('%linux%'), 'Linux'),
+        else_='Other'
+    ).label('operating_system')
+
+    bucket = time_bucket(duration, EventModel.time)
+    lookup_pages = pages if isinstance(pages, list) and len(pages) > 0 else DEFAULT_LOOKUP_PAGES
+    query = (
+        select(
+            bucket.label('bucket'),
+            os_case,
+            EventModel.page.label('page'),
+            func.avg(EventModel.duration).label("avg_duration"),
+            func.count().label('count')
+        )
+        .where(
+            EventModel.page.in_(lookup_pages)
+        )
+        .group_by(
+            bucket,
+            os_case,
+            EventModel.page,
+        )
+        .order_by(
+            bucket,
+            os_case,
+            EventModel.page,
+        )
+    )
+    results = session.exec(query).fetchall()
+    return results
+
+# SEND DATA HERE
+# create view
+# POST /api/events/
+@router.post("/", response_model=EventModel)
+def create_event(
+        payload:EventCreateSchema, 
+        session: Session = Depends(get_session)):
+    # a bunch of items in a table
+    data = payload.model_dump() # payload -> dict -> pydantic
+    obj = EventModel.model_validate(data)
+    session.add(obj)
+    session.commit()
+    session.refresh(obj)
+    return obj
+
+
+# GET /api/events/12
+@router.get("/{event_id}", response_model=EventModel)
+def get_event(event_id:int, session: Session = Depends(get_session)):
+    # a single row
+    query = select(EventModel).where(EventModel.id == event_id)
+    result = session.exec(query).first()
+    if not result:
+        raise HTTPException(status_code=404, detail="Event not found")
+    return result
